9ba0780754e2c47c2bbb169c27e462e9e1ac7966
cresence2k24 <cresence@jntugvcev.edu.in>
2025-02-11 19:13:30 +0530

    a

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "http://cresence.vercel.app", 
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
